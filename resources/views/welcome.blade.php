c2f50eb3680ee1f2e500f3e87d2462de8490960a
fmaple <fmaple163@163.com>
2017-10-18 18:41:33 +0800

    testagin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-test
+                    Laravel-test-ag
                 </div>
 
                 <div class="links">
