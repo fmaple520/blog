4ab1858383475a97d236d069b764b0c5d390ba05
fmaple <fmaple163@163.com>
2017-10-18 20:37:51 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-test-aa
+                    Laravel-blog
                 </div>
 
                 <div class="links">
