ad3960f7d653d3adac06d79a2e5309c62cd06632
fmaple <fmaple163@163.com>
2018-05-17 20:23:19 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-blog
+                    Laravel-fmaple-blog
                 </div>
 
                 <div class="links">
